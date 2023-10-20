2d60924155c462b2c0236a0344a220776cddaaa4
akhileshravikumar <akhi2431@gmail.com>
2023-10-20 11:34:29 +0530

    fetching meals from backend, instead of dummy data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,60 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Card from "../UI/Card";
-// import axios from 'axios'
 import MealItem from "./MealItem/MealItem";
 import classes from './AvailableMeals.module.css'
 
-// const options = {
-//   method: 'GET',  
-//   url: 'https://foodiefetch.p.rapidapi.com/swiggy',
-//   params: {
-//     query: 'grandamas cafe pune'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': '55dbd089b7msh804effbbdca4777p19b20ejsn2a2738669a46',
-//     'X-RapidAPI-Host': 'foodiefetch.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
-
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
-
 const AvailableMeals=()=>{
+  // const [isLoading,setIsLoading] = useState(false);
+  const [meals,setMeals] = useState([]);
+
+
+  useEffect(()=>{
+    const fetchMeals = async ()=>{
+      const response = await fetch('https://react-meals-db-3b512-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for(const key in responseData){
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          price: responseData[key].price,
+          description: responseData[key].description
+        })
+      }
+      setMeals(loadedMeals);
+    }
+    fetchMeals();
+  },[]);
+
+
     return(<section className={classes.meals}> 
         <Card>
             <ul>
-                {DUMMY_MEALS.map((item)=>{return(<MealItem 
+                {meals.map((item)=>{return(<MealItem 
                 key={item.id}
                 id={item.id}
                 price={item.price}
